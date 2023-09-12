532df95e408f5626926422fc49ab5699f7c3a0c8
Jan Bock <jeevo162@gmail.com>
2023-09-12 17:05:48 +0100

    setup for landing page

--- typescript-react-redux_web/src/App.tsx
+++ typescript-react-redux_web/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import LandingPage from './app/screens/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <p>hi</p>
-      </header>
+        <LandingPage />
     </div>
   );
 }
